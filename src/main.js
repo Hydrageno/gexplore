2aad7690c2851d7ef4e939e682217525f2864f7d
Hydrageno <3062055813@qq.com>
2023-03-12 14:42:44 +0800

    import component from 'element-ui' unit-test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Button, Select} from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
 
 new Vue({
   render: h => h(App),

e072f5250e653755c6e76e004d34bcef89f59961
Hydrageno <3062055813@qq.com>
2023-03-12 20:32:57 +0800

    add Tabs、TabPane component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {Button, Row} from 'element-ui'
+import {Button, Row, Tabs, TabPane} from 'element-ui'
 
 
 Vue.config.productionTip = false
 Vue.component(Button.name, Button)
 Vue.component(Row.name, Row)
+Vue.component(Tabs.name, Tabs)
+Vue.component(TabPane.name, TabPane)
 
 
 new Vue({

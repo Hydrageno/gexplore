4fea58cfa03b315d0961aef18c7280b1ea7928c6
Hydrageno <3062055813@qq.com>
2023-03-13 01:27:19 +0800

    m-p: import external compont from element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {Button, Row, Tabs, TabPane} from 'element-ui'
+import {Button, Row, Tabs, TabPane, Table} from 'element-ui'
 
 
 Vue.config.productionTip = false
@@ -8,6 +8,7 @@ Vue.component(Button.name, Button)
 Vue.component(Row.name, Row)
 Vue.component(Tabs.name, Tabs)
 Vue.component(TabPane.name, TabPane)
+Vue.component(Table.name, Table)
 
 
 new Vue({

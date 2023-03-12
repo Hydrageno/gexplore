614efc9244acff6b7eaf6e1bf441b3889128eb2c
Hydrageno <3062055813@qq.com>
2023-03-12 16:04:12 +0800

    a-c: init switchbutton component

---
+++ src/components/SwitchButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button> {{ value }} </button>
+</template>
+
+<script>
+export default{
+    name: 'SwitchButton',
+    props: ["value"]  // 
+}
+</script>
+
+<style lang="less">
+
+</style>

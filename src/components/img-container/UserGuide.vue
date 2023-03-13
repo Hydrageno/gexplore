5fc51a75a0f7f2a156d2c5b6a674d84bc1b58dbe
Hydrageno <3062055813@qq.com>
2023-03-14 01:15:26 +0800

    a-c: user-guide to tell user function of the div

---
+++ src/components/img-container/UserGuide.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="userGuideArea">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'UserGuide'
+}
+</script>
+
+<style lang="less">
+.userGuideArea{
+    top: 0;
+    left: 0;
+    position: absolute;
+    z-index: 1;
+    border-style: groove;
+    border-color: #E5E7EB;
+    border-top-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    color: rgb(107,116,156);
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+</style>

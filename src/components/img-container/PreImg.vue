0d8ea49fcad6886647ae5e2f919a13672947eddb
Hydrageno <3062055813@qq.com>
2023-03-14 01:14:41 +0800

    m-p: basci style of pre-img finished yet.

--- src/components/img-container/PreImg.vue
+++ src/components/img-container/PreImg.vue
@@ -1,14 +1,59 @@
 <template>
-    <div class="imgWorkingArea">
+    <div class="imgWorkingArea">  
+        <user-guide>&nbsp;&nbsp;<img src="../../assets/userguide.svg" height="17">input image&nbsp;</user-guide>
+        <el-upload class="imgUploader" drag action="https://jsonplaceholder.typicode.com/posts">
+            <div class="descriptionArea">
+                <i class="el-icon-upload"></i>
+                <div class="descriptionTextArea">
+                    "Click to Upload"
+                        - - or - -
+                    "Drop Image Here"
+                </div>
+            </div>
+        </el-upload>     
     </div>
 </template>
 
 <script>
+import UserGuide from './UserGuide.vue';
 export default{
-    name: 'PreImg'
+    name: 'PreImg',
+    components: {
+        UserGuide
+    }
 }
 </script>
 
 <style lang="less">
 @import "../../assets/ContainerStyle.less";
+.imgWorkingArea{ 
+    cursor: pointer;  // set cursor style
+    position: relative;
+    .imgUploader{
+        height: 100%;  // succeed parent's height
+        width: 100%;  // succeed parent's width
+        .el-upload, 
+        .el-upload-dragger{
+            height: 100%;
+            width: 100%;
+            color: rgb(107,116,156);
+            border-style: none;
+        }
+        .el-upload:hover, 
+        .el-upload-dragger:hover{
+            color: black;
+        }
+        .descriptionArea{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 100%;
+            .descriptionTextArea{
+                text-align: center;
+            }
+        }   
+    }
+}
 </style>

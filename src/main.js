34ffd9309691f4ee9fb9d080033e3b82b0143d79
Hydrageno <3062055813@qq.com>
2023-03-13 14:57:20 +0800

    re: import tablecolumn from element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {Button, Row, Tabs, TabPane, Table} from 'element-ui'
+import {Button, Row, Tabs, TabPane, Table, TableColumn} from 'element-ui'
 
 
 Vue.config.productionTip = false
@@ -9,6 +9,7 @@ Vue.component(Row.name, Row)
 Vue.component(Tabs.name, Tabs)
 Vue.component(TabPane.name, TabPane)
 Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
 
 
 new Vue({

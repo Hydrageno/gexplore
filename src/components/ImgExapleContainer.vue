e5926f0a5523ad5238f51640b33446cc8a31f701
Hydrageno <3062055813@qq.com>
2023-03-13 14:58:42 +0800

    m-p: init img ex. can

--- src/components/ImgExapleContainer.vue
+++ src/components/ImgExapleContainer.vue
@@ -1,10 +1,27 @@
 <template>
-    <div id="imgExapleContainerArea"></div>
+    <div id="imgExapleContainerArea" :data="imgExample">
+        <el-table :data="tableData" stripe>
+            <el-table-column prop="date" label="date" width="180"></el-table-column>
+            <el-table-column prop="name" label="name" width="180"></el-table-column>
+            <el-table-column prop="address" label="address"></el-table-column>
+        </el-table>
+    </div>
 </template>
 
 <script>
 export default{
     name: 'ImgExapleContainer',
+    components:{
+    },
+    data(){
+        return {
+        tableData: [{
+          date: '2016-05-02',
+          name: 'sev',
+          address: 'German'
+        }]
+      }
+    }
 }
 </script>
 

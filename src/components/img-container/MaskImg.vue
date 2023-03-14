0442c9861305bc42b431b82a0d5bd2245078a9e3
Hydrageno <3062055813@qq.com>
2023-03-15 00:44:17 +0800

    m-p: add mask.icon & complete maskImg component.

--- src/components/img-container/MaskImg.vue
+++ src/components/img-container/MaskImg.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="imgArea maskImgArea"></div>
+    <div class="imgArea maskImgArea">
+        <user-guide>&nbsp;&nbsp;<img src="../../assets/mask.svg" height="17">mask_image&nbsp;</user-guide>
+    </div>
 </template>
 
 <script>
+import UserGuide from './UserGuide.vue';
 export default{
-    name: 'MaskImg'
+    name: 'MaskImg',
+    components: {
+        UserGuide
+    }
 }
 </script>
 
 <style lang="less">
 @import "../../assets/ContainerStyle.less";
-
 </style>

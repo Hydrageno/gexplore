723dcd3ed29fd2d0d1b47c51468c0e9f128398f8
Hydrageno <3062055813@qq.com>
2023-03-15 01:15:41 +0800

    m-p: complete maskImg component & add event

--- src/components/img-container/MaskImg.vue
+++ src/components/img-container/MaskImg.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="imgArea maskImgArea">
-        <user-guide>&nbsp;&nbsp;<img src="../../assets/mask.svg" height="17">mask_image&nbsp;</user-guide>
+        <user-guide>&nbsp;&nbsp;<img src="../../assets/mask.svg" height="17">mask_image:
+            <el-button size="mini" @click="maskGenerator()">Mask</el-button>&nbsp;&nbsp;&nbsp;
+        </user-guide>
     </div>
 </template>
 
@@ -10,10 +12,18 @@ export default{
     name: 'MaskImg',
     components: {
         UserGuide
+    },
+    methods: {
+        maskGenerator:function(){
+            console.log("yes")
+        }
     }
 }
 </script>
 
 <style lang="less">
 @import "../../assets/ContainerStyle.less";
+user-guide{
+    overflow: hidden;
+}
 </style>
